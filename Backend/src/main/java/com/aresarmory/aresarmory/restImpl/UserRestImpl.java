7e71d27c0487d2d86180e69122d02d8b14122b78
soitlaur <60842345+soitlaur@users.noreply.github.com>
2023-11-01 23:18:59 +0200

    [BE] Added CORS Configuration

--- Backend/src/main/java/com/aresarmory/aresarmory/restImpl/UserRestImpl.java
+++ Backend/src/main/java/com/aresarmory/aresarmory/restImpl/UserRestImpl.java
@@ -7,6 +7,7 @@ import com.aresarmory.aresarmory.utils.ArmoryUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
