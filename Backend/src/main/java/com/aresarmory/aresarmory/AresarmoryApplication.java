6cb071b60240d12817a9c0cec8351bb8bed4e061
soitlaur <60842345+soitlaur@users.noreply.github.com>
2023-11-02 00:18:32 +0200

    Revenit

--- Backend/src/main/java/com/aresarmory/aresarmory/AresarmoryApplication.java
+++ Backend/src/main/java/com/aresarmory/aresarmory/AresarmoryApplication.java
@@ -2,9 +2,6 @@ package com.aresarmory.aresarmory;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class AresarmoryApplication {
@@ -12,4 +9,5 @@ public class AresarmoryApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(AresarmoryApplication.class, args);
 	}
+
 }
--- Backend/src/main/java/com/aresarmory/aresarmory/restImpl/UserRestImpl.java
+++ Backend/src/main/java/com/aresarmory/aresarmory/restImpl/UserRestImpl.java
@@ -7,12 +7,10 @@ import com.aresarmory.aresarmory.utils.ArmoryUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
 
-@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 public class UserRestImpl implements UserRest {
 
