4f464ebf71eb477c1313a8223360b5f42f21be44
Laurențiu Șoit <60842345+LaurentiuSoit@users.noreply.github.com>
2023-11-01 20:45:08 +0200

    Add files via upload
    [BE] Added signup functionality

---
+++ Backend/src/main/java/com/aresarmory/aresarmory/serviceImpl/UserServiceImpl.java
@@ -0,0 +1,58 @@
+package com.aresarmory.aresarmory.serviceImpl;
+
+import com.aresarmory.aresarmory.POJO.User;
+import com.aresarmory.aresarmory.constants.ArmoryConstants;
+import com.aresarmory.aresarmory.dao.UserDao;
+import com.aresarmory.aresarmory.service.UserService;
+import com.aresarmory.aresarmory.utils.ArmoryUtils;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import java.util.Map;
+import java.util.Objects;
+
+@Slf4j
+@Service
+public class UserServiceImpl implements UserService {
+    @Autowired
+    UserDao userDao;
+
+    @Override
+    public ResponseEntity<String> signUp(Map<String, String> requestMap) {
+        log.info("Inside signup {}", requestMap);
+        try {
+            if (validateSignUpMap(requestMap)) {
+                User user = userDao.findByEmailId(requestMap.get("email"));
+                if (Objects.isNull(user)) {
+                    userDao.save(getUserFromMap(requestMap));
+                    return ArmoryUtils.getResponseEntity("Successfully Registered", HttpStatus.OK);
+                } else {
+                    return ArmoryUtils.getResponseEntity("Email already exists", HttpStatus.BAD_REQUEST);
+                }
+            } else
+                return ArmoryUtils.getResponseEntity(ArmoryConstants.INVALID_DATA, HttpStatus.BAD_REQUEST);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        return ArmoryUtils.getResponseEntity(ArmoryConstants.SOMETHING_WENT_WRONG, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    private boolean validateSignUpMap(Map<String, String> requestMap) {
+        return requestMap.containsKey("name") && requestMap.containsKey("contactNumber")
+                && requestMap.containsKey("email") && requestMap.containsKey("password");
+    }
+
+    private User getUserFromMap(Map<String, String> requestMap) {
+        User user = new User();
+        user.setName(requestMap.get("name"));
+        user.setContactNumber(requestMap.get("contactNumber"));
+        user.setEmail(requestMap.get("email"));
+        user.setPassword(requestMap.get("password"));
+        user.setStatus("false");
+        user.setRole("user");
+        return user;
+    }
+}
---
+++ Backend/src/test/java/com/aresarmory/aresarmory/AresarmoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aresarmory.aresarmory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AresarmoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
