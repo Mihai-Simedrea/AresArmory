03a0a9aab801e2b75a0654cd33955d621f7c07ca
Laurențiu Șoit <60842345+LaurentiuSoit@users.noreply.github.com>
2023-11-01 21:20:52 +0200

    [BE] Added User table and signup functionality

---
+++ Backend/src/main/java/com/aresarmory/aresarmory/constants/ArmoryConstants.java
@@ -0,0 +1,7 @@
+package com.aresarmory.aresarmory.constants;
+
+public class ArmoryConstants {
+
+    public static final String SOMETHING_WENT_WRONG = "Something Went Wrong";
+    public static final String INVALID_DATA = "Invalid Data";
+}
