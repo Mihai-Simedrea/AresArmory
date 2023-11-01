cc936c50b1017065c04611a1243773d8d49dd707
soitlaur <60842345+soitlaur@users.noreply.github.com>
2023-11-01 23:54:06 +0200

    Se incearca CORS

--- Backend/src/main/java/com/aresarmory/aresarmory/JWT/JwtUtil.java
+++ Backend/src/main/java/com/aresarmory/aresarmory/JWT/JwtUtil.java
@@ -1,8 +1,34 @@
 package com.aresarmory.aresarmory.JWT;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
 import org.springframework.stereotype.Service;
+
+import java.util.Date;
+import java.util.function.Function;
 
 @Service
 public class JwtUtil {
+    private String secret = "aresarmory";
 
+    public String extractUsername(String token){
+        return extractClaims(token, Claims::getSubject);
+    }
+
+    public Date extractExpiration(String token){
+        return extractClaims(token, Claims::getExpiration);
+    }
+
+    public <T> T extractClaims(String token, Function<Claims,T> claimsResolver){
+        final Claims claims = extractAllClaims(token);
+        return claimsResolver.apply(claims);
+    }
+
+    public Claims extractAllClaims(String token){
+        return Jwts.parser().setSigningKey(secret).parseClaimsJws(token).getBody();
+    }
+
+    private Boolean isTokenExpired(String token){
+        return extractExpiration(token).before(new Date());
+    }
 }
