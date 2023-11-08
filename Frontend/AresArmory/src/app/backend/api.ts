c06e1877dc834e617e8f1b0aa9747fd3264d61ec
MihaiSimedrea <mihai.nicolae.simedrea@gmail.com>
2023-11-08 22:51:27 +0200

    [FE] Add Login Functionality

--- Frontend/AresArmory/src/app/backend/api.ts
+++ Frontend/AresArmory/src/app/backend/api.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
 import { AuthModel } from '../components/shared/models/auth.model';
+import { LoginModel } from '../components/shared/models/login.model';
 import { environment } from '../environments/environment';
 
 @Injectable({
@@ -14,5 +15,10 @@ export class AuthClient {
 
   signUp(authModel: AuthModel): Observable<any> {
     return this.http.post<any>(`${this.baseUrl}/signup`, authModel);
+  }
+  
+  login(loginModel: LoginModel): Observable<any> {
+    // return this.http.get<any>(`${this.baseUrl}/login`, loginModel);
+    return of();
   }
 }
--- Frontend/AresArmory/src/app/pages/login/login.component.ts
+++ Frontend/AresArmory/src/app/pages/login/login.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthClient } from 'src/app/backend/api';
+import { LoginModel } from 'src/app/components/shared/models/login.model';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
   imagePath: string = '../../../assets/79403162-8cd3-4052-a8c8-d69831c48ce4.jpg';
+  loginModel: LoginModel;
+
+  constructor(private readonly authClient: AuthClient) {
+    this.loginModel = new LoginModel();
+  }
+
+  ngOnInit(): void {
+
+  }
+
+  login(): void {
+    console.log(this.loginModel);
+    
+    this.authClient.login(this.loginModel).subscribe(result => {
+      console.log(result);
+    },
+    () => {
+      console.log('error');
+    });
+  }
 }
