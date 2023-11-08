105ffbcc11592fcfddc8069285e185f2cd9b81d1
MihaiSimedrea <mihai.nicolae.simedrea@gmail.com>
2023-11-08 23:40:26 +0200

    [FE] Add Logout Functionality

--- Frontend/AresArmory/src/app/components/header/header.component.ts
+++ Frontend/AresArmory/src/app/components/header/header.component.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+
+  constructor(private readonly router: Router) {
+
+  }
+
+  ngOnInit(): void {
+
+  }
+
   logout(): void {
-    
+    localStorage.clear();
+    this.router.navigate(['/login']);
   }
 }
