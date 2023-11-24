e8ab05bdbe59935a6df8c6ef3fa796a1b97287a0
Mihai-Simedrea <mihai.nicolae.simedrea@gmail.com>
2023-11-24 02:19:54 +0200

    [FE] Add About Me section

--- Frontend/AresArmory/src/app/pages/contact/contact.component.ts
+++ Frontend/AresArmory/src/app/pages/contact/contact.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
   styleUrls: ['./contact.component.scss']
 })
-export class ContactComponent {
+export class ContactComponent implements OnInit {
+  userName!: string;
+  userEmail!: string;
+  userRole!: string;
+  userInitials: string = '';
 
+  ngOnInit(): void {
+    this.userName = localStorage.getItem("username")!;
+    this.userEmail = localStorage.getItem("email")!;
+    this.userRole = localStorage.getItem("role")!;
+    this.userInitials = this.calculateInitials(this.userName);
+  }
+
+  calculateInitials(name: string): string {
+    if (!name) {
+      return '';
+    }
+
+    const words = name.split(' ');
+    const initials = words
+      .map(word => word.charAt(0).toUpperCase())
+      .join('');
+
+    return initials;
+  }
 }
